79aebdd6f7e29869eee862f0cdf2e43e50e43cc3
Nikhil Ganjir <nikhilganjir2411@gmail.com>
2026-05-26 13:48:18 +0530

    Completed all sections under Who We Are

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),  // 👈 MAIN LINE
+      '@': path.resolve(__dirname, './src'), 
     },
   },
 })
